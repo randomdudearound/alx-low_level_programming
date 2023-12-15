3040e03e2734477003020244020c0e03245da122
randomdudearound <super.r200@yahoo.com>
2023-12-15 03:55:33 +0200

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* main: Entry point
+*main: Entry point
 *
-* Return: always 0 (success)
+*Return: always 0 (success)
 */
 int main (void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
 }
